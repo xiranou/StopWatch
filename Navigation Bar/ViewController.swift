dc884a22c7810c2313bbe534ff7188d8db2a3c58
xiranou <xiranou@gmail.com>
2016-01-11 16:38:17 -0500

    initial app

--- Navigation Bar/ViewController.swift
+++ Navigation Bar/ViewController.swift
@@ -9,17 +9,81 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var toolBar: UIToolbar!
+    var playButton = UIBarButtonItem()
+    var pauseButton = UIBarButtonItem()
+    var buttonsArray = [UIBarButtonItem]()
+    
+    var seconds = 0
+    var timer = NSTimer()
+    var isCounting = false
+    
+    func increaseTimer() {
+        seconds++
+        displayTimerText()
+    }
+    
+    func displayTimerText() {
+        let (h, m, s) = secondsToHMS(seconds)
+        timerLabel.text = "\(formatTimerText(h)):\(formatTimerText(m)):\(formatTimerText(s))"
+    }
+
+    func formatTimerText(time: Int) -> String {
+        return time >= 10 ? "\(time)" : "0\(time)"
+    }
+    
+    func secondsToHMS(seconds: Int) -> (Int, Int, Int) {
+        return (seconds/3600, (seconds%3600)/60, (seconds%3600)%60)
+    }
+    
+    func displayPlayPauseButton() {
+        buttonsArray = self.toolBar.items!
+        buttonsArray.removeAtIndex(0)
+        
+        if (isCounting) {
+            buttonsArray.insert(pauseButton, atIndex: 0)
+        } else {
+            buttonsArray.insert(playButton, atIndex: 0)
+        }
+        
+        self.toolBar.setItems(buttonsArray, animated: false)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        playButton = UIBarButtonItem(barButtonSystemItem: .Play, target: self, action: "play")
+        pauseButton = UIBarButtonItem(barButtonSystemItem: .Pause, target: self, action: "pause")
+        displayPlayPauseButton()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    func play() {
+        isCounting = true
+        displayPlayPauseButton()
+        timerLabel.textColor = UIColor.greenColor()
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "increaseTimer", userInfo: nil, repeats: true)
+    }
 
+    func pause() {
+        isCounting = false
+        displayPlayPauseButton()
+        timerLabel.textColor = UIColor.redColor()
+        timer.invalidate()
+    }
+    
+    @IBAction func reset(sender: AnyObject) {
+        isCounting = false
+        displayPlayPauseButton()
+        seconds = 0
+        timerLabel.textColor = UIColor.blackColor()
+        timer.invalidate()
+        displayTimerText()
+    }
 }
 
